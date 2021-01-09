37779d3ff6c8cddaf39e1e7cddd8b60767ec5323
Thiago Alves da Costa <thiago.alves.ec@gmail.com>
2021-01-09 09:37:05 -0300

    Ready to prod (fixing heroku issue)

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { OrderPayLoad } from "./types";
+import { OrderPayLoad } from "./Orders/types";
 
 const  API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
